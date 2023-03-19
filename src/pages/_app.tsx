732b8b1bbb07ee71a8d86a910b56fa47430026c8
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-03-18 23:59:40 -0500

    feat: add app font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app'
+import { Noto_Sans } from 'next/font/google'
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap'
+})
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={notoSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
