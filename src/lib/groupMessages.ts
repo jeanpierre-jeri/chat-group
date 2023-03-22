5838d76438bb140518286a00f90e2de4a06e3aed
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-03-22 00:38:42 -0500

    feat: format time in messages

--- src/lib/groupMessages.ts
+++ src/lib/groupMessages.ts
@@ -1,14 +1,4 @@
-import { ChatMessage } from '@/components/organisms'
-
-const getDay = (date: string) => {
-  const newDate = new Date(date)
-
-  return new Intl.DateTimeFormat('en-US', {
-    month: 'long',
-    day: '2-digit',
-    year: 'numeric'
-  }).format(newDate)
-}
+import type { ChatMessage } from '@/hooks'
 
 export type MessageWithUser = {
   id: string
@@ -20,7 +10,7 @@ export type MessageWithUser = {
   }
 }
 
-export const groupMessages = (messages: MessageWithUser[]): ChatMessage[] => {
+export function groupMessages(messages: MessageWithUser[]): ChatMessage[] {
   const newMessages = new Map()
 
   messages.forEach(({ created_at, id, content, users }) => {
@@ -30,15 +20,55 @@ export const groupMessages = (messages: MessageWithUser[]): ChatMessage[] => {
     if (!mapDateMessage) {
       newMessages.set(day, {
         day,
-        messages: [{ created_at, id, content, users }]
+        messages: [{ created_at: formatTime(created_at), id, content, users }]
       })
 
       return
     }
 
-    mapDateMessage.messages.unshift({ created_at, id, content, users })
+    mapDateMessage.messages.unshift({
+      created_at: formatTime(created_at),
+      id,
+      content,
+      users
+    })
     newMessages.set(day, mapDateMessage)
   })
 
   return Array.from(newMessages.values())
+}
+
+function getDay(date: string) {
+  const newDate = new Date(date)
+
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric'
+  }).format(newDate)
+}
+
+function formatTime(date: string): string {
+  const newDate = new Date(date)
+
+  const time = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(newDate)
+  if (newDate.toDateString() === new Date().toDateString())
+    return `today at ${time}`
+
+  const yesterday = new Date()
+  yesterday.setDate(yesterday.getDate() - 1)
+
+  if (yesterday.toDateString() === newDate.toDateString())
+    return `yesterday at ${time}`
+
+  return new Intl.DateTimeFormat('en-US', {
+    month: '2-digit',
+    year: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(newDate)
 }
